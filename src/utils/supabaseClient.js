48ad72475e3113073ad5ff497f7e58a5d2c6d9eb
Richard_cyber <128294864+rihard-cyber@users.noreply.github.com>
2026-05-13 17:22:03 +0700

    SUPABASE_SERVICE_ROLE_KEY

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://your-project-url.supabase.co';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'your-anon-key';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://bhauqlobuiuavaoeoawc.supabase.co';
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_USBAIKjq_bdE21mRGoxVyA_XGibd9ai';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
